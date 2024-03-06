3e4896227b2f02567378d9f8246c31a6b3e0dbfd
Peepat <128441401+SoHizz21@users.noreply.github.com>
2024-03-06 14:41:45 +0000

    POST

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify ,request
 from flask_cors import CORS,cross_origin
 
 app = Flask(__name__)
@@ -11,15 +11,23 @@ books=[
     {"id":3,"title":"Book 3","author":"Author 3"}
 ]
 
+@app.route("/")
+@cross_origin()
+def hello_world():
+    return  "<h1>Hello_world</h1>"
+
 @app.route("/books",methods=["GET"])
 @cross_origin()
 def get_all_books():
     return jsonify({"books":books})
 
-@app.route("/")
+@app.route("/books",methods=["POST"])
 @cross_origin()
-def hello_world():
-    return  "<h1>Hello_world</h1>"
+def insert_id_book():
+    new_book = request.get_json()
+    books.append(new_book)
+    return jsonify({"books": books})
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=5000,debug=True)
